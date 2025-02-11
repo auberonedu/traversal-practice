d09b1fcae964627839ab8af6b2b86ccff3372b01
Augy Markham <katiefaithmarkham@gmail.com>
2025-02-11 14:46:33 -0800

    finished printOddNodes

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -8,7 +8,22 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printOddNodes(Node<Integer> node) {
+    /* if node null return
+     * check left
+     * check right
+     * 
+     * if node.value % 2 != 0
+     *  print value
+     */
+    if (node == null){
+      return;
+    }
 
+    printOddNodes(node.left);
+    printOddNodes(node.right);
+    if (node.value % 2 != 0){
+      System.out.println(node.value);
+    }
   }
 
   /**
