cb72be035a5dc38a9bfb982b2f6db3a5889ff788
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-02-11 14:47:57 -0800

    Finished printOddNodes method

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -8,7 +8,15 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static void printOddNodes(Node<Integer> node) {
+    if (node == null) {
+      return;
+    }
 
+    if (node.value % 2 != 0) {
+      System.out.println(node.value);
+    }
+      printOddNodes(node.left);
+      printOddNodes(node.right);
   }
 
   /**
